eb360ab16a67751e4a1d90882530de1bea5a473d
Peter Anderson <peanderson@ea.com>
2019-04-27 16:08:59 +0100

    adding missing files

---
+++ storage-blobs-dotnet-quickstart/Program.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+//MIT License
+
+//Copyright(c) 2017 Microsoft Corporation. All rights reserved.
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+//------------------------------------------------------------------------------
+
+
+namespace storage_blobs_dotnet_quickstart
+{
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Azure Blob storage - .NET Quickstart sample");
+            Console.WriteLine();
+
+            var dirPath = args[0];
+            if (!Directory.Exists(dirPath))
+            {
+                Console.WriteLine("Directory does not exists: ", dirPath);
+                return 1;
+            }
+
+            var files = Directory.GetFiles(args[0]);
+            foreach (String file in files)
+            {
+                ProcessAsync(file).GetAwaiter().GetResult();
+            }
+
+            Console.WriteLine("Press any key to exit the sample application.");
+            Console.ReadLine();
+            return 0;
+        }
+
+        private static async Task ProcessAsync(string sourceFile)
+        {
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=iothackshm;AccountKey=FQ2yQ59uInxiGRsBUTURPPA4lb/wbx38KRzfTOWLtAXADejNA9KYRQ+uTEyIAT0j1+ZippOM1o6xWzBpkwDt0Q==;EndpointSuffix=core.windows.net";
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    // Create a container called 'quickstartblobs' and append a GUID value to it to make the name unique. 
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("quickstartblobs");
+                    await cloudBlobContainer.CreateIfNotExistsAsync();
+
+                    // Set the permissions so the blobs are public. 
+                    BlobContainerPermissions permissions = new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    };
+                    await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                    // Write text to the file.
+                    File.WriteAllText(sourceFile, "Hello, World!");
+
+                    Console.WriteLine("Temp file = {0}", sourceFile);
+                    Console.WriteLine("Uploading to Blob storage as blob '{0}'", sourceFile);
+                    Console.WriteLine();
+
+                    string fileName = Path.GetFileNameWithoutExtension(sourceFile);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    // Use the value of localFileName for the blob name.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(fileName);
+                    await cloudBlockBlob.UploadFromFileAsync(sourceFile);
+
+                    Console.WriteLine("Done uploading!");
+                    // do app specific stuff 
+                }
+                catch (StorageException ex)
+                {
+                    Console.WriteLine("Error returned from the service: {0}", ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine(
+                    "A connection string has not been defined in the system environment variables. " +
+                    "Add a environment variable named 'storageconnectionstring' with your storage " +
+                    "connection string as a value.");
+            }
+        }
+    }
+}
